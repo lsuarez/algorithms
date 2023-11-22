e2dd8afb5c59125a6cd4e1e86a6f1bd1b7e8271b
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2023-11-21 18:06:56 -0600

    Removing recursion

--- algorithms/src/com/linked/list/SumList.java
+++ algorithms/src/com/linked/list/SumList.java
@@ -1,60 +1,93 @@
 package com.linked.list;
 
 public class SumList {
+    static LinkedList result = new LinkedList();
+    static int carry =0;
     public static void main(String args[]){
-        Node l1 = new Node(7);
-        l1.appendToTail(1);
-        l1.appendToTail(6);
-        Node l2 = new Node(5);
-        l2.appendToTail(9);
-        l2.appendToTail(2);
+        LinkedList l1 = new LinkedList();
+        l1.append(5);
+        l1.append(1);
+        l1.append(2);
 
-        Node result = sumList(l1, l2);
-        System.out.println(result);
+        int size =l1.getSize();
+        LinkedList l2 = new LinkedList();
+        l2.append(1);
+        l2.append(2);
+        l2.append(5);
+        l2.append(9);
+
+        LinkedList result = numValues(l1, l2);
 
     }
-    private static Node sumList(Node l1, Node l2){
+
+    private static LinkedList numValues(LinkedList l1, LinkedList l2){
         if(l1 == null && l2 == null ){
             return null;
         }
-        Node result = new Node();
-        boolean isHead = true;
-        int carry=0;
+        LinkedList result = new LinkedList();
 
-        Node next1 = l1, next2 =l2;
-        while(next1 != null || next2!=null){
-            int value = carry;
-            value += next1.data + next2.data;
-            next1 = next1.next;
-            next2 = next2.next;
-            if(isHead) {
-                result = new Node(value % 10);
-                isHead= false;
-            } else
-                result.appendToTail(value%10);
+
+        Node next1 = l1.head, next2 = l2.head;
+        int carry =0, value=0;
+        while(next1!= null && next2!= null) {
+            value=next1.data+next2.data+carry;
+            result.append(value % 10);
             if(value>=10)
                 carry=1;
             else
                 carry =0;
+            next1=next1.next;
+            next2=next2.next;
+        }
+        while(next1!=null){
+            result.append(next1.data);
+            next1=next1.next;
+        }
+        while(next2!=null){
+            result.append(next2.data);
+            next2=next2.next;
         }
         return result;
     }
 
-}
 
-class Node{
-    Node next =null;
-    int data;
-    public Node(int d){
-        data =d;
-    }
-    public Node(){}
-    void appendToTail(int d){
-        Node end = new Node(d);
-        Node n = this;
-        while(n.next != null){
-            n = n.next;
+
+
+}
+class LinkedList {
+    Node head;
+
+    private int size =0;
+
+    void append( int d) {
+        Node newNode = new Node(d);
+        if (size != 0) {
+            newNode.next = this.head;
         }
-        n.next = end;
+        this.head = newNode;
+        size++;
+    }
+    int getByPosition(int pos){
+        int value =-1;
+        if(pos>size)
+            return value;
+        Node current = this.head;
+        Node temp = null;
+return 0;
+    }
+
+
+    int getSize(){
+        return this.size;
+    }
+
+}
+class Node {
+    Node next = null;
+    int data;
+    public Node(int d) {
+        this.data=d;
+    }
+    public Node() {
     }
 }
