eac3122a1e17a5e3f84953a72bd1e3c55eb0e860
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2023-11-22 14:30:02 -0600

    Removing secondary list

--- algorithms/src/com/linked/list/Palindrome.java
+++ algorithms/src/com/linked/list/Palindrome.java
@@ -2,16 +2,18 @@ package com.linked.list;
 
 public class Palindrome {
 
-    public static boolean isPalindrome(LinkedList l1, LinkedList l2){
+    public static boolean isPalindrome(LinkedList l1){
+        Node l2Node = l1.head;
         Node l1Node = reverseList(l1.head);
-        Node l2Node = l2.head;
+
         while(l1Node !=null && l2Node !=null){
             if(l1Node.data != l2Node.data)
                 return false;
             l1Node = l1Node.next;
             l2Node = l2Node.next;
         }
-        return true;
+
+        return l1Node == null && l2Node ==null;
     }
     private static Node reverseList(Node node){
 
@@ -26,17 +28,13 @@ public class Palindrome {
     }
     public static void main(String args[]){
         LinkedList l1 = new LinkedList();
-        l1.append(2);
-        l1.append(29);
-        l1.append(7);
-        l1.append(8);
-        LinkedList l2 = new LinkedList();
-        l2.append(8);
-        l2.append(7);
-        l2.append(29);
-        l2.append(2);
+        l1.append(1);
+        l1.append(0);
+        l1.append(3);
+        l1.append(1);
+
 
         //Success scenario
-        System.out.println(isPalindrome(l1, l2));
+        System.out.println(isPalindrome(l1));
     }
 }
