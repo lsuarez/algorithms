94abc2edf3b50339165ff335108ac008866d2903
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2023-11-27 22:41:24 -0500

    Adding test

--- algorithms/src/com/stacks/StackOfPlates.java
+++ algorithms/src/com/stacks/StackOfPlates.java
@@ -15,9 +15,9 @@ public class StackOfPlates {
         stack.push(1);
         stack.push(2);
         stack.push(3);
-        stack.pop();
-        stack.pop();
-        stack.pop();
-        stack.pop();
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
     }
 }
