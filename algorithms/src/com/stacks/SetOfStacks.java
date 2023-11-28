75d31ee17ae6d2b4d5c3994b11328af666708847
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2023-11-27 22:36:10 -0500

    Creating stack of stacks

---
+++ algorithms/src/com/stacks/SetOfStacks.java
@@ -0,0 +1,81 @@
+package com.stacks;
+
+import java.util.EmptyStackException;
+
+public class SetOfStacks {
+    private SetOfStacks next;
+    private StackNode top;
+    private static final int maxCapacity=2;
+    private int currentCapacity=0;
+    public void push(int data){
+        if(top==null) {
+            top = new StackNode(data);
+            currentCapacity ++;
+        }
+        else if(currentCapacity< maxCapacity) {
+            StackNode t = new StackNode(data);
+            t.next = this.top;
+            this.top = t;
+            currentCapacity ++;
+        }else{
+            SetOfStacks currentStack = getNextStack(this);
+            StackNode t = new StackNode(data);
+            t.next = currentStack.top;
+            currentStack.top = t;
+            SetOfStacks lastStack = getLastStack();
+           // this.next = currentStack;
+            currentStack.currentCapacity ++;
+
+            if(currentStack.currentCapacity<currentStack.maxCapacity)
+                lastStack.next = currentStack;
+
+        }
+
+    }
+
+    private SetOfStacks getLastStack() {
+        SetOfStacks stack = this;
+        while(stack.currentCapacity == stack.maxCapacity  && stack.next!= null){
+            stack=stack.next;
+        }
+        return stack;
+    }
+
+
+    private SetOfStacks getNextStack(SetOfStacks stack){
+        stack = stack.next;
+        if(stack == null){
+            return new SetOfStacks();
+        }
+        if(stack.currentCapacity<stack.maxCapacity){
+            return stack;
+        }else{
+            return getNextStack(stack);
+        }
+    }
+    private class StackNode{
+        private int data;
+        private StackNode next;
+
+        public StackNode(int data){
+            this.data = data;
+        }
+    }
+    private SetOfStacks getLastStackForPop() {
+        SetOfStacks stack = this;
+        while( stack.next!= null && stack.next.currentCapacity>0){
+            stack=stack.next;
+        }
+        return stack;
+    }
+    public int pop(){
+        if(this.currentCapacity<=0)
+            throw new EmptyStackException();
+        SetOfStacks currentStack = getLastStackForPop();
+        int value = currentStack.top.data;
+        currentStack.top = currentStack.top.next;
+        currentStack.currentCapacity--;
+        return value;
+    }
+
+}
---
+++ algorithms/src/com/stacks/StackOfPlates.java
@@ -0,0 +1,23 @@
+package com.stacks;
+
+/**
+ * Imagine a(literal) stack of plates. If the stack gets too high, it might topple.
+ * Therefore, in the real life, we would likely start a new stack when the previous stack exceeds some threshold.
+ * Implement a data structure SetOfStacks that mimics this.
+ * SetOfStacks should be composed of several stacks and should create a new stack once the previous one exceeds
+ * capacity. SetOfStacks push() and pop() should behave identically to a single stack (that is, pop() should
+ * return the same values as it would if there were just a single stack).
+ */
+
+public class StackOfPlates {
+    public static void main(String args[]){
+        SetOfStacks stack = new SetOfStacks();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+    }
+}
