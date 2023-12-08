5810d59ec9c56c9a5f0fc573926ecbb6048671c4
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2023-12-07 19:07:40 -0600

    Adding fix in sorting with recursive method

--- algorithms/src/com/sort/SortWithRecursion.java
+++ algorithms/src/com/sort/SortWithRecursion.java
@@ -1,18 +1,20 @@
 package com.sort;
 
 public class SortWithRecursion {
-    public static int[] sort(int a[], int low, int high){
+    private static int[] sort(int a[], int low, int high){
+
+
         if(low>=high || high> a.length+1 || low<0 )
             return a;
         int pivot = (high+low)/2;
-        if(pivot-1 <0 || pivot+1 >a.length-1)
-            return a;
-        if(a[pivot-1]> a[pivot])
+
+        if(pivot-1>=0 && a[pivot-1]> a[pivot])
             a=makeSwitch(a, pivot-1, pivot);
-        if(a[pivot] > a[pivot+1])
+        if(pivot+1<=a.length-1 && a[pivot] > a[pivot+1]  )
             a=makeSwitch(a, pivot, pivot+1);
         sort(a, pivot+1, high);
         sort(a, 0, pivot);
+        sort(a, pivot+1, high);
         return a;
     }
     private static int[] makeSwitch(int a[], int index1, int index2){
@@ -34,8 +36,15 @@ public class SortWithRecursion {
 
         System.out.println();
         int a1[]={9,4,2,7,6,12,3,90,1};
-        a=sort(a1, 0, a1.length);
+        a1=sort(a1, 0, a1.length);
         print(a1);
+        //Output: 1 2 3 4 6 9 7 12 90
+
+        System.out.println();
+        int a2[]={11,10,9,8,7,6,5,4,3,2,1};
+        a2=sort(a2, 0, a2.length);
+        a2=sort(a2, 0, a2.length);
+        print(a2);
         //Output: 1 2 3 4 6 9 7 12 90
     }
 }
