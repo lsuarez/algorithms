3f40e7606f6081c187cdb48893c1f772506dc1fb
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2023-12-09 15:25:25 -0600

    Adding exponent calculation fix

--- algorithms/src/com/recursion/Exponent.java
+++ algorithms/src/com/recursion/Exponent.java
@@ -4,30 +4,31 @@ package com.recursion;
          */
 
 public class Exponent {
+    private static int array[];
     private static int calculate(int base, int exp){
-        if(exp == 1)
+        if(exp == 1) {
+            array[0]=base;
             return base;
-        if(exp==2)
-            return base*base;
-        if(exp ==3)
-            return base * calculate(base, 2);
-       int result =  calculate(base, exp/2);
-       return result * result;
+        }
+       int result =  calculate(base, exp-1);
+        array[exp-1]=base* result;
+       return array[exp-1];
     }
     public static int exponent(int base, int exp){
-        if(base<=0 || exp<=0)
+        array = new int[exp];
+        if(base<=0 || exp == 0)
             return -1;
         if(exp == 1)
             return base;
-        int result =  calculate(base, exp);
-        if(exp%2!=0)
-            result=result * base;
-        return result;
+        int result =  calculate(base, exp/2);
+        if(exp%2==0)
+            return result*result;
+        return result *result * base;
     }
     public static void main(String args[]){
-        System.out.println("Calculate exponent of 2 to 8 ="+exponent(2, 8)); //Calculate exponent of 2 to 8 =256
+        System.out.println("Calculate exponent of 2 to 14 ="+exponent(2, 14)); //Calculate exponent of 2 to 8 =256
         System.out.println("Calculate exponent of 3 to 5 ="+exponent(3, 5)); //Calculate exponent of 3 to 5 =243
-        System.out.println("Calculate exponent of 2 to 6 ="+exponent(2, 6)); //Calculate exponent of 2 to 6 =16
+        System.out.println("Calculate exponent of 2 to 10 ="+exponent(2, 10)); //Calculate exponent of 2 to 6 =16
         System.out.println("Calculate exponent of 4 to 1 ="+exponent(4, 1)); //Calculate exponent of 4 to 1 =4
         System.out.println("Calculate exponent of 4 to 1 ="+exponent(4, 0)); //Calculate exponent of 4 to 1 =-1
     }
