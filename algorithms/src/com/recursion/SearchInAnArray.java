4599fbdc4565d61b19e59f0bdf6985668cec035a
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2023-12-10 20:22:52 -0600

    Adding search in logs

--- algorithms/src/com/recursion/SearchInAnArray.java
+++ algorithms/src/com/recursion/SearchInAnArray.java
@@ -27,9 +27,15 @@ public class SearchInAnArray {
      */
 
     public static int searchPosition(boolean array[], int low, int high){
+        if(array==null || array.length==0)
+            return -1;
+        if(low == high )
+            return -1;
         int n=array.length;
         int mid = low+(high-low)/2;
         if(!array[mid]) {
+            if(mid == 0)
+                return -1;
             if (mid-1 <= n && array[mid-1])
                 return mid - 1;
             else
@@ -48,5 +54,14 @@ public class SearchInAnArray {
         //retornar 11
         boolean[] log2 ={true, true, true, true, true, true,true, true, true,true, true, true, false, false};
         System.out.println(searchPosition(log2, 0, log2.length));
+
+        // [false, false, false, false]
+        // -1
+        boolean[] log3 ={false, false, false, false};
+        System.out.println(searchPosition(log3, 0, log3.length));
+        // []
+        // -1
+        boolean[] log4 ={};
+        System.out.println(searchPosition(log4, 0, log4.length));
     }
 }
