1fe46fb6d2be3fe35ec64237de783f824421e7ef
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2023-12-26 22:24:00 -0600

    Adding common prefix solution

--- algorithms/src/com/challenge/CommonPrefix.java
+++ algorithms/src/com/challenge/CommonPrefix.java
@@ -11,29 +11,29 @@ public class CommonPrefix {
             return "";
         if(strs[0].isEmpty())
             return "";
-        return searchCommonPrefix(strs[0].substring(0,1), 1,  1, strs);
-    }
-    private static String searchCommonPrefix(String prefix, int i, int index, String[] strs){
-        if((strs[0].length()<=index && i>=strs.length ) || (strs[i].length()<=index  && i>=strs.length)) {
-            if(i>=strs.length)
-                return prefix;
-            if (prefix.equals(strs[i].substring(0, index)))
-                return prefix;
-            else
-                return prefix.substring(0, index - 1);
+        StringBuilder sb= new StringBuilder();
+        int arrayLen = strs.length;
+        int index=1;
+        for(char l:strs[0].toCharArray()){
+            int i=1;
+            sb.append(l);
+            while (arrayLen>i){
+                if((strs[i].length()<=index  || i>=strs[i].length() )&& arrayLen==i) {
+                    if(sb.toString().equals(strs[i].substring(0,index)))
+                        return sb.toString();
+                    else
+                        return "";
+                }
+                if( strs[i].length()>=index && sb.toString().equals(strs[i].substring(0,index)))
+                    i++;
+                else
+                    return sb.toString().substring(0,index-1);
+            }
+            index++;
         }
-        if(i>= strs.length-1 && strs[0].length()>index) {
-            if(i>= strs.length-1 && strs[0].length()==index)
-                return searchCommonPrefix(prefix, i + 1, index, strs);
-            return searchCommonPrefix(strs[0].substring(0, index + 1), 1, index + 1, strs);
-        }
-        if(prefix.equals(strs[i].substring(0, index)))
-            return searchCommonPrefix(prefix, i + 1, index, strs);
-        else if (prefix.length()<1)
-            return "";
-        else
-            return prefix.substring(0,index-1);
+        return sb.toString();
     }
+
     public static void main(String args[]){
 
         //["flower","flow","flight"] -> fl
@@ -42,8 +42,16 @@ public class CommonPrefix {
         String[] str= {"ab","a"}; //fl
         String[] str2={"flower","flow","flight"};
         String[] str3={"flower","flower","flower","flower"};
+        String[] str4= {"a","b"}; // ""
+        String[] str5= {"a","a","b"}; // ""
+        System.out.println(longestCommonPrefix(str5));
         System.out.println(longestCommonPrefix(str));
-       // System.out.println(longestCommonPrefix(str2));
-       // System.out.println(longestCommonPrefix(str3));
+        System.out.println(longestCommonPrefix(str2));
+        System.out.println(longestCommonPrefix(str3));
+
+
+        System.out.println(longestCommonPrefix(str4));
+
+
     }
 }
