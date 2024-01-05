c81cdaebe332e75017b0158c6372d49f1d630dde
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2024-01-04 19:01:40 -0600

    Adding reverse linkedlist

--- algorithms/src/com/linked/list/ReverseList.java
+++ algorithms/src/com/linked/list/ReverseList.java
@@ -3,6 +3,7 @@ package com.linked.list;
 public class ReverseList {
 
     static Node head = null;
+    static boolean first=true;
     public static Node reverse(Node node){
         while(node!= null) {
             if (head == null) {
@@ -16,13 +17,31 @@ public class ReverseList {
         }
         return head;
     }
+
+    public static Node reverseInSameList(Node node){
+       Node tmp = null;
+       Node current = node;
+       Node next = null;
+       while(current != null){
+           next = current.next;
+           current.next = tmp;
+           tmp = current;
+           current = next;
+       }
+       node = tmp;
+       return node;
+    }
+
+
     public static void main(String args[]){
         Node node = new Node(1);
         Node node2= new Node(2);
         Node node3= new Node(3);
         node2.next=node3;
         node.next= node2;
-        reverse(node);
+       // reverse(node);
+        Node reverse = reverseInSameList(node);
+        System.out.println(reverse);
 
     }
 }
