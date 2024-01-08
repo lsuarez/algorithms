2fe05cceaf2bc9d02fe5240ea54de5a2c61ec89d
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2024-01-08 17:30:44 -0600

    Adding transposicion

--- algorithms/src/com/arrays/Transposition.java
+++ algorithms/src/com/arrays/Transposition.java
@@ -3,50 +3,51 @@ package com.arrays;
 public class Transposition {
 
     public static void printArray(int [][] a){
-        for(int col=0; col< a[0].length; col++){
-            for(int row=0; row<a.length; row++){
+        for(int col=0; col< a.length; col++){
+            for(int row=0; row<a[0].length ; row++){
                 System.out.print(a[col][row] + " ");
             }
             System.out.println();
         }
     }
-    public static int [][] transposition(int [][] a){
-        System.out.println("****");
+
+    public static void transposition(int [][] a){
+        System.out.println("transposition");
         for(int col=0; col< a[0].length; col++){
-            for(int row=col; row<a.length; row++){
-               int temp = a[col][row];
-                a[col][row] = a[row][col];
-                a[row][col] = temp;
+
+            int r=0;
+            while(r<= a[0].length){
+                System.out.print(a[r][col] +" ");
+                r++;
             }
+            System.out.println();
         }
-        return a;
     }
 
     public static void main(String args[]){
-        int[][] array = new int[3][3];
+        int[][] array = new int[3][2];
         array[0][0] = 1;
-        array[0][1] = 1;
-        array[0][2] = 1;
+        array[0][1] = 2;
+       // array[0][2] = 1;
 
-        array[1][0] = 2;
-        array[1][1] = 2;
-        array[1][2] = 2;
+        array[1][0] = 3;
+        array[1][1] = 4;
+       // array[1][2] = 2;
 
-        array[2][0] = 3;
-        array[2][1] = 3;
-        array[2][2] = 3;
+        array[2][0] = 5;
+        array[2][1] = 6;
+       // array[2][2] = 3;
         printArray(array);
-        array = transposition(array);
-        printArray(array);
+        transposition(array);
+      //  printArray(array);
 
         /*
-1 1 1
-2 2 2
-3 3 3
-****
-1 2 3
-1 2 3
-1 2 3
+3 4
+5 6
+transposition
+1 3 5
+2 4 6
+
          */
     }
 }
