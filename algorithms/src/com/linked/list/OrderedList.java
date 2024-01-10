b4304f90b37754d3f4d318f526fbc812e28ae850
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2024-01-09 18:52:20 -0600

    Ordering a list

--- algorithms/src/com/linked/list/OrderedList.java
+++ algorithms/src/com/linked/list/OrderedList.java
@@ -1,33 +1,59 @@
 package com.linked.list;
 
 public class OrderedList {
-    public static Node orderingList(Node node){
-        Node tmp = null;
-        Node current = node;
-        Node next = null;
-        Node prev = node;
-        while(current != null){
-            next = current.next;
-            if(next!= null && next.data<current.data) {
-                tmp = current;
-                current = next;
-                tmp.next = next.next;
-                current.next = tmp;
-            }
-            current = next;
-        }
+    public static Node ordering(Node node){
+            Node tmp = null;
+            Node current = node;
+            Node next = null;
+            Node prev = node;
+            boolean firstSkipe = false;
+            while(current != null){
 
+                next = current.next;
+                if(next!= null && next.data<current.data) {
+                    tmp = current;
+                    current = next;
+                    tmp.next = next.next;
+                    current.next = tmp;
+                    if (node == tmp) {
+                        node = current;
+                    }else{
+                        prev.next=current;
+                    }
+                    prev = current;
+
+                }else{
+                    if(firstSkipe){
+                        prev = prev.next;
+                        firstSkipe=false;
+                    }
+                    firstSkipe=true;
+                }
+
+                current = next;
+
+            }
+            return node;
+    }
+    public static Node orderList(Node node){
+        Node current = node;
+        while(current!=null){
+            node =ordering(node);
+            current = current.next;
+        }
         return node;
     }
+
     public static void main(String args[]){
-        Node node = new Node(9);
-        Node node2= new Node(7);
-        Node node3= new Node(8);
+        Node node = new Node(10);
+        Node node2= new Node(8);
+        Node node3= new Node(11);
+        Node node4= new Node(7);
+        node3.next=node4;
         node2.next=node3;
         node.next= node2;
-        // reverse(node);
-        Node reverse = orderingList(node);
-        System.out.println(reverse);
+        Node ordered = orderList(node);
+        System.out.println(ordered);
 
     }
 }
