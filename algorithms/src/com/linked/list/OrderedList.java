8b67cecb903016be8f1971808e2017e32357f5c6
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2024-01-10 12:14:53 -0600

    Ordering a list

--- algorithms/src/com/linked/list/OrderedList.java
+++ algorithms/src/com/linked/list/OrderedList.java
@@ -1,58 +1,20 @@
 package com.linked.list;
 
 public class OrderedList {
-    public static Node ordering(Node node){
-            Node tmp = null;
-            Node current = node;
-            Node next = null;
-            Node prev = node;
-            boolean firstSkipe = false;
-            while(current != null){
 
-                next = current.next;
-                if(next!= null && next.data<current.data) {
-                    tmp = current;
-                    current = next;
-                    tmp.next = next.next;
-                    current.next = tmp;
-                    if (node == tmp) {
-                        node = current;
-                    }else{
-                        prev.next=current;
-                    }
-                    prev = current;
-
-                }else{
-                    if(firstSkipe){
-                        prev = prev.next;
-                        firstSkipe=false;
-                    }
-                    firstSkipe=true;
-                }
-
-                current = next;
-
-            }
-            return node;
-    }
-    public static Node orderList(Node node){
+    private static Node removingANode(Node node, int toRemove){
         Node current = node;
-        while(current!=null){
-            node =ordering(node);
-            current = current.next;
-        }
-        return node;
-    }
-
-    public static Node removingANode(Node node, int toRemove){
-        Node current = node;
-        Node prev = null;
+        Node prev = node;
         Node temp=null;
         while(current!=null){
             if(current.data == toRemove){
                 temp = current.next;
-                prev.next =temp;
-                current =prev;
+                if(prev == current){
+                    return temp;
+                }else {
+                    prev.next = temp;
+                }
+
                 return node;
             }
             prev= current;
@@ -60,18 +22,61 @@ public class OrderedList {
         }
         return node;
     }
+    private static Node gatherBiggerOne(Node node){
+        int data=Integer.MIN_VALUE;
+        Node current = node;
+        Node bigger = null;
+        while(current !=null){
+            if(current.data>data ){
+                data = current.data;
+                bigger = current;
+            }
+            current = current.next;
+        }
+        return bigger;
+    }
+
+    public static Node orderingList(Node node){
+        Node current = node;
+        Node prev = null;
+        Node tmp = null;
+        while(current!=null){
+            tmp = prev;
+            prev = gatherBiggerOne(node);
+            node = removingANode(node, prev.data);
+            current=current.next;
+            if(tmp != null){
+                prev.next = tmp;
+            }else{
+                prev.next= null;
+            }
+            if(current == null){
+                tmp = prev;
+                prev= node;
+                prev.next = tmp;
+            }
+        }
+        return prev;
+    }
+
+    public static void printList(Node node){
+        Node current = node;
+        while(current!=null){
+            System.out.println(current.data);
+            current=current.next;
+        }
+    }
 
     public static void main(String args[]){
-        Node node = new Node(10);
-        Node node2= new Node(8);
+        Node node = new Node(10); //10-> 8-> 11->7
+        Node node2= new Node(8); // 7->8->10->11
         Node node3= new Node(11);
         Node node4= new Node(7);
         node3.next=node4;
         node2.next=node3;
         node.next= node2;
-        Node newNode = removingANode(node, 11);
-      //  Node ordered = orderList(node);
-        System.out.println(newNode);
+
+        printList(orderingList(node));
 
     }
 }
