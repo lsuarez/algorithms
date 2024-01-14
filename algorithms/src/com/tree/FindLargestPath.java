e7e165de7af731f6b613020ca155819e2a20d07c
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2024-01-13 22:27:42 -0600

    Find largest path

--- algorithms/src/com/tree/FindLargestPath.java
+++ algorithms/src/com/tree/FindLargestPath.java
@@ -45,19 +45,30 @@ public class FindLargestPath {
     }
 
     public static void printLargestPath(BinaryNode bt) {
+        if(bt==null){
+            System.out.println("There are not nodes");
+        }
+        if(bt.getLeft() == null && bt.getRight() == null){
+            System.out.println("Largest path is:" +bt.getData());
+            return;
+        }
         root = bt.getRight().getData();
-        searchingLargestPath(bt.getLeft());
+        if(bt.getLeft() != null) {
+            searchingLargestPath(bt.getLeft());
+        }
         largestPath = new StringBuilder();
         counter = 1;
-        searchingLargestPath(bt.getRight());
+        if(bt.getRight() != null) {
+            searchingLargestPath(bt.getRight());
+        }
         System.out.println("Largest path is:" + pathTemp + bt.getData());
 
     }
 
     public static void main(String[] args) {
-        BinaryNode tn =
-                new BinaryNode(new BinaryNode(null, new BinaryNode(3), 9), new BinaryNode(new BinaryNode(8), new BinaryNode(null, new BinaryNode(1), 2), 7), 5);
-
+     //   BinaryNode tn =
+        //        new BinaryNode(new BinaryNode(null, new BinaryNode(3), 9), new BinaryNode(new BinaryNode(8), new BinaryNode(null, new BinaryNode(1), 2), 7), 5);
+        BinaryNode tn = new BinaryNode(null, new BinaryNode(null, new BinaryNode(3),4),2);
         printLargestPath(tn);
 
   /*
