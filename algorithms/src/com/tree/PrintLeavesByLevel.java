91142eeee1bcb2b5203dac8712e3b016e0885930
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2024-01-15 22:11:04 -0600

    Printing

--- algorithms/src/com/tree/PrintLeavesByLevel.java
+++ algorithms/src/com/tree/PrintLeavesByLevel.java
@@ -3,34 +3,50 @@ package com.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class PrintLeavesByLevel {
 
 
-    static List<String> levels= new ArrayList<>();
-    public static void printLeavesByLevel(BinaryNode node, int level){
+    static int level = 0;
+    static List<BinaryNode> nodes = new CopyOnWriteArrayList<>();
 
-        if(node == null){
-            level=0;
+    private static void printChildren(BinaryNode node) {
+
+        if (node.getLeft() != null) {
+            System.out.print(node.getLeft().getData() + " ");
+            nodes.add(node.getLeft());
+        }
+        if (node.getRight() != null) {
+            System.out.print(node.getRight().getData() + " ");
+            nodes.add(node.getRight());
+        }
+
+    }
+
+    public static void printingWithoutRecursion(BinaryNode bn) {
+        if (bn == null) {
             return;
         }
-        String history;
-        if(levels.size()==level){
-            levels.add(node.getData()+"");
-        }else {
-            history = levels.get(level) + " ";
-            levels.remove(levels.get(level));
-            levels.add(level, history + node.getData());
+        System.out.println(bn.getData());
+
+        int i = 0;
+        printChildren(bn);
+        for (BinaryNode n : nodes) {
+            i = i + 2;
+            double enter = Math.pow(2, i - 1);
+            if (i == enter) {
+                System.out.println();
+            }
+            printChildren(n);
         }
-        level++;
-        printLeavesByLevel(node.getLeft(), level);
-        printLeavesByLevel(node.getRight(),level);
+
     }
+
     public static void main(String[] args) {
         BinaryNode tn =
-                new BinaryNode(new BinaryNode(new BinaryNode(3), new BinaryNode(5),4) , new BinaryNode(new BinaryNode(12), new BinaryNode(null, null,9),8), 11);
-        printLeavesByLevel(tn,0);
-        levels.stream().forEach(System.out::println);
+                new BinaryNode(new BinaryNode(new BinaryNode(3), new BinaryNode(5), 4), new BinaryNode(new BinaryNode(12), new BinaryNode(null, null, 9), 8), 11);
+        printingWithoutRecursion(tn);
         /*
         11
     4   |  8
