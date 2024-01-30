a25621947dc86098f705c754e3642a8fd7d83ff3
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2024-01-30 12:46:16 -0600

    Fix

--- algorithms/src/com/arrays/Permutation.java
+++ algorithms/src/com/arrays/Permutation.java
@@ -1,37 +1,48 @@
 package com.arrays;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-public class GetPermutations {
 
-    private static List findPermutation(int[]a, int index){
-        if(index< a.length && index+1<= a.length) {
-            int temp = a[index];
-            a[index]=a[index+1];
-            a[index+1]=temp;
-            return Arrays.asList(a);
-        }
-        return null;
-    }
-    public static List<List<Integer>> getPermutations(int[]a){
+public class Permutation {
+
+    public  List<List<Integer>> getPermutations(int[]a){
         if(a==null || a.length==0) {
             return null;
         }
-        findPermutation(a, a[0]);
-        return null;
+        int index =0;
+        List<List<Integer>> permutations = new ArrayList<>();
+        findPermutation( index, a, permutations);
+
+        return permutations;
+    }
+
+    private void findPermutation(int i, int[] nums, List<List<Integer>> result) {
+        if (i == nums.length - 1) {
+            List<Integer> list = new ArrayList<>();
+            for (int n : nums) list.add(n);
+            result.add(list);
+        } else {
+            for (int j = i, l = nums.length; j < l; j++) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                findPermutation(i + 1, nums, result);
+                temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
     }
     public static void main(String args[]){
-        System.out.println("Holaaaaaaaaaaa");
-
-    }
-    /*
-     int[]array= new int[3];
+        int[]array= new int[3];
         array[0]=1;
         array[1]=2;
         array[2]=3;
-        System.out.println(array);
-        List list =getPermutations(array);
-        System.out.println(list.toString());
-     */
+
+        Permutation obj = new Permutation();
+        List list =obj.getPermutations(array);
+        System.out.println(list.toString()); //[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]
+
+    }
+
 }
