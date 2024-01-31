acb22219e3fb9f084534562bdbcd0f7c5a053416
Liz Suarez <lizsuarez@Lizs-MacBook-Pro.local>
2024-01-30 19:51:55 -0600

    Adding random

--- algorithms/src/com/challenge/Random.java
+++ algorithms/src/com/challenge/Random.java
@@ -4,10 +4,12 @@ package com.challenge;
 
 public class Random {
     public int getValue(int N){
+        String nStr= N+"";
+        int lenOfN= nStr.length();
         long nano =System.nanoTime();
         String valueStr= nano+"";
         int max = valueStr.length();
-        String sub = valueStr.substring(max-2, max-1);
+        String sub = valueStr.substring(max-lenOfN, max);
         int value = Integer.valueOf(sub);
         value = value % (N+1);
         return value;
@@ -15,10 +17,10 @@ public class Random {
 
     public static void main(String[] args) {
         Random obj= new Random();
-        System.out.println(obj.getValue(3));
-        System.out.println(obj.getValue(3));
-        System.out.println(obj.getValue(3));
-        System.out.println(obj.getValue(3));
+        System.out.println(obj.getValue(1500));
+        System.out.println(obj.getValue(1500));
+        System.out.println(obj.getValue(1500));
+        System.out.println(obj.getValue(1500));
         System.out.println(obj.getValue(3));
         System.out.println(obj.getValue(3));
         System.out.println(obj.getValue(3));
